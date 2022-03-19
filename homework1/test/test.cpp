eabaf421afbd51a18bd1bedd35ec6fedd3e5271d
canlvshaonian <1521417179@qq.com>
2022-03-20 01:24:17 +0800

    添加了识别标识符和关键字的部分

--- homework1/test/test.cpp
+++ homework1/test/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <cctype>
 
 using namespace std;
@@ -7,12 +8,14 @@ int main()
 {
     string digit = "1";
     string alpha = "bss";
+    string key[2] = {"bss","nb"};
+    char test[4] = "bss";
 
     const char* cdigit = digit.c_str();
     const char* calpha = alpha.c_str();
 
+    cout<<key[1].c_str()<<endl;
 
-
-    cout<<cdigit<<isdigit(*cdigit)<<endl;
-    cout<<calpha<<isalpha(*calpha)<<endl;
+    cout << strcmp(test, key[0].c_str()) << endl;
+    cout << strcmp(test, key[1].c_str()) << endl;
 }
