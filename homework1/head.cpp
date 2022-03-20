25615343134cb516ee2e27d28bfceba974df5dab
canlvshaonian <1521417179@qq.com>
2022-03-20 17:04:28 +0800

    完成了词法分析器部分

--- homework1/head.cpp
+++ homework1/head.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-void scanner::goback()
-{
-    pos--;
-}
-
-int scanner::ifsymbol(char ch)
-{
-    switch(ch){
-        case '+':
-            token[tokenpos++] = ch;
-            ch = getnextchar();
-            if(isdigit(ch))
-                return 28;
-            else
-            {
-                pos--;
-                type = 
-            }
-    }
-}
-
 char scanner::getnextchar()
 {
     if(pos < buffer.length())
@@ -64,26 +43,75 @@ void scanner::gettoken()
                         state = 4;
                     else if(ispunct(ch))
                         state = 6;
+                    else if (ch == ' ' || ch == '\t' || ch == '\n')
+                        state = 100;
                     else
                         state = -1;
                 case 1:
                     token[tokenpos++] = ch;
                     ch = getnextchar();
-                    if(isalpha(ch)||isdigit(ch))
+                    if (isalpha(ch) || isdigit(ch))
                         state = 1;
                     else
                     {
                         pos--;
                         state = 5;
                     }
-                case 5:
-                    int i;
-                    for(i = 0;i<32;i++)
+                    break;
+                case 2:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if(ch == '\'')
                     {
-                        if(strcmp(token, keytable[i].c_str()) == 0)
+                        state = 100;
+                        type = 2;
+                    }
+                    else
+                    {
+                        token[tokenpos++] = ch;
+                        ch = getnextchar();
+                        if (ch == '\'')
                         {
                             state = 100;
-                            type = keynum[i];
+                            type = 2;
+                        }
+                        else
+                        {
+                            pos--;
+                            state = -1;
+                        }
+                    }
+                    break;
+                case 3:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if (ch == '"')
+                        state = 12;
+                    else
+                        state = 3;
+                    break;
+                case 4:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if (isdigit(ch))
+                        state = 4;
+                    else if (ch == '.')
+                        state = 7;
+                    else
+                    {
+                        pos--;
+                        state = 100;
+                        type = 4;
+                    }
+                    break;    
+                case 5:
+                    int i;
+                    for (i = 0;i<32;i++)
+                    {
+                        if (strcmp(token, keytable[i].c_str()) == 0)
+                        {
+                            state = 100;
+                            type = keytype[i];
                             break;
                         }
                     }
@@ -93,8 +121,146 @@ void scanner::gettoken()
                         type = 1;
                     }
                     break;
-
+                case 6:
+                    if (ch == '\'')
+                        state = 2;
+                    else if (ch == '"')
+                        state = 3;
+                    else if (ch == '/')
+                        state = 11;
+                    else
+                    {
+                        int i;
+                        for (i = 0; i < 41; i++)
+                        {
+                            if(strcmp(token, symboltable[i].c_str()) == 0)
+                            {
+                                state = 100;
+                                type = symboltype[i];
+                                break;
+                            }
+                        }
+                        if(i == 41)
+                        {
+                            state = -1;
+                        }
+                    }
+                    break;
+                case 7:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if (isdigit(ch))
+                        state = 7;
+                    else if (ch == 'e')
+                        state = 8;
+                    else
+                    {
+                        pos--;
+                        state = 100;
+                        type = 37;
+                    }
+                    break;
+                case 8:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if(isdigit(ch))
+                        state = 9;
+                    else if(ch == '-')
+                        state = 10;
+                    else
+                    {
+                        pos--;
+                        state = -1;
+                    }
+                    break;
+                case 9:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if(isdigit(ch))
+                        state = 9;
+                    else
+                    {
+                        pos--;
+                        state = 100;
+                        type = 37;
+                    }
+                    break;
+                case 10:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if (isdigit(ch))
+                        state = 9;
+                    else
+                    {
+                        pos--;
+                        state = -1;
+                    }
+                    break;
+                case 11:
+                    token[tokenpos++] = ch;
+                    ch = getnextchar();
+                    if (ch == '/')
+                        state = 13;
+                    else if(ch == '*')
+                        state = 14;
+                    else
+                    {
+                        pos--;
+                        state = 100;
+                        type = symboltype[13];
+                    }
+                    break;
+                case 12:
+                    token[tokenpos++] = ch;
+                    state = 100;
+                    type = 3;
+                    break;
+                case 13:
+                    while((ch = getnextchar()) != '\n');
+                    state = 100;
+                    type = 79;
+                    break;
+                case 14:
+                    ch = getnextchar();
+                    if(ch == '*')
+                        state = 15;
+                    else
+                        state = 14;
+                    break;
+                case 15:
+                    ch = getnextchar();
+                    if (ch == '*')
+                        state = 15;
+                    else if(ch == '/')
+                        state = 16;
+                    else
+                        state = 14;
+                    break;
+                case 16:
+                    state = 100;
+                    type = 80;
+                    break;
+                case -1:
+                    cout<<"error"<<endl;
+                    std::cout << (int)ch << " " << ch << std::endl;
+				    ch = getnextchar();
+				    while (ch != ' '&&ch != '\t'&&ch != '\n'&&ch != ';') 
+                        ch = getnextchar();
+                    pos--;
+                    state = 100;
+                    type = -1;
+            }
+            if(state = 100 && type != -1)
+            {
+                outfile<<"("<<token<<","<<type<<")"<<endl;
+                memset(token, 0, tokenlength);
+                tokenpos = 0;
+                state = 0;
+                type = -1;
             }
         }
+        if (state == 100)
+            state = 0;
     }
+    outfile.close();
 }
--- homework1/head.h
+++ homework1/head.h
@@ -12,12 +12,13 @@ private:
     std::string buffer;
     const char* filename;
     std::ifstream infile;
-    char symboltable[17] = {'+','-','*','/',
-                            '>','<','=',';',
-                            '!','[',']','{',
-                            '}','(',')','"',
-                            ','};
-    int symbolnum[17] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    std::string symboltable[41] = {"(", ")", "[", "]", "->", ".", "!", "~",
+                                   "++", "--", "-", "*", "&", "/", "%", "+",
+                                   "<<", ">>", "<", "<=", ">", ">=", "==", "!=",
+                                   "^", "|", "&&", "||", "?:", "=", "+=", "-=",
+                                   "*=", "/=", "%=", "<<=", ">>=", "&=", "^=", "|=",
+                                   ","};
+    int symboltype[41] = {38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78};
     std::string keytable[32] = {"auto", "break", "case", "char",
                                 "const", "continue", "default", "do",
                                 "double", "else", "enum", "extern",
@@ -26,7 +27,7 @@ private:
                                 "short","signed","sizeof","static",
                                 "struct","switch","typedef","union",
                                 "unsigned","void","volatile","while"};
-    int keynum[32] = {5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+    int keytype[32] = {5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
 public:
     scanner(const char* s)
     {
@@ -41,11 +42,7 @@ public:
     {
         infile.close();
     }
-
-    void goback();
-
-    int ifsymbol(char ch);
-
+    
     void gettoken();
 
     char getnextchar();
